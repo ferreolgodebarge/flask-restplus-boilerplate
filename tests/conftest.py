1830ef662ae8b0497260bbba3b20352665310ac3
GODEBARGE Ferréol <ferreol.godebarge@wavestone.com>
2019-06-09 14:13:34 +0200

    Change test_settings.cfg location

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,7 +21,12 @@ def app_no_env():
 def app_no_env_with_file():
     if 'APP_ENV' in os.environ:
         os.environ.pop('APP_ENV')
-    test_settings = os.path.join(os.getcwd(), 'tests', 'test_settings.cfg')
+    test_settings = os.path.join(
+        os.getcwd(),
+        'tests',
+        'test_files',
+        'test_settings.cfg',
+    )
     settings = os.path.join(os.getcwd(), 'settings.cfg')
     shutil.copyfile(test_settings, settings)
     yield create_app()
@@ -36,7 +41,12 @@ def create_app_test():
 @pytest.fixture
 def app_dev_with_config_file():
     os.environ['APP_ENV'] = 'DEV'
-    test_settings = os.path.join(os.getcwd(), 'tests', 'test_settings.cfg')
+    test_settings = os.path.join(
+        os.getcwd(),
+        'tests',
+        'test_files',
+        'test_settings.cfg',
+    )
     settings = os.path.join(os.getcwd(), 'settings.cfg')
     shutil.copyfile(test_settings, settings)
     yield create_app()
@@ -52,7 +62,12 @@ def client():
 
 @pytest.fixture
 def client_v1():
-    test_settings = os.path.join(os.getcwd(), 'tests', 'test_settings.cfg')
+    test_settings = os.path.join(
+        os.getcwd(),
+        'tests',
+        'test_files',
+        'test_settings.cfg',
+    )
     settings = os.path.join(os.getcwd(), 'settings.cfg')
     shutil.copyfile(test_settings, settings)
     app = create_app()
@@ -63,7 +78,12 @@ def client_v1():
 
 @pytest.fixture
 def client_v1_backend_issue(monkeypatch):
-    test_settings = os.path.join(os.getcwd(), 'tests', 'test_settings.cfg')
+    test_settings = os.path.join(
+        os.getcwd(),
+        'tests',
+        'test_files',
+        'test_settings.cfg',
+    )
     settings = os.path.join(os.getcwd(), 'settings.cfg')
     shutil.copyfile(test_settings, settings)
     monkeypatch.setattr("application.models.init_app", lambda x: '')
